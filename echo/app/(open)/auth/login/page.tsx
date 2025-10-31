6a4db5aa3f006afe7d02639b2578e541f5600a5b
ChamithChathurya-Helixz <chamith.chathurya@helixzsolutions.com>
2025-10-31 18:47:50 +0530

    initial setup

---
+++ echo/app/(open)/auth/login/page.tsx
@@ -0,0 +1,9 @@
+import Login from "@/modules/component/Login";
+
+export default function LoginPage() {
+    return (
+        <main className="flex min-h-screen items-center justify-center bg-gray-50 p-4">
+            <Login redirectTo="/dashboard" />
+        </main>
+    );
+}
