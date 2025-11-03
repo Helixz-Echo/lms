ebd1531ec498f21648884843e47b2d7a358ad65f
ChamithChathurya-Helixz <chamith.chathurya@helixzsolutions.com>
2025-11-03 10:21:19 +0530

    feat:add missing file

---
+++ echo/app/dashboard/admin/upload/page.tsx
@@ -0,0 +1,9 @@
+import Uploader from "@/modules/component/Uploader";
+
+export default function UploadPage() {
+    return (
+        <div className="h-screen flex items-center justify-center">
+            <Uploader />
+        </div>
+    );
+}
